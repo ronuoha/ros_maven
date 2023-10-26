bc6bb08682b0c18f9ded7b85f9dfd258df644ed7
Roselyne Loves <roselyneloves@sir.com>
2023-10-26 18:18:50 -0500

    adding new files

---
+++ src/main/java/com/ibt/ros-app/App.java
@@ -0,0 +1,13 @@
+package com.ibt.ros_app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World this is Ros from Houston!" );
+    }
+}
